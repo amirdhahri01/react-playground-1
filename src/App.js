10d4e7d60a4f1458febda740900f380f13a18ab6
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-06 22:58:46 +0100

    An update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import AboutUs from "./components/about";
+import Navbar from "./components/navbar"
+import Users from "./components/users"
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
+        <Route path="/" element={<Navbar/>}>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/users" element={<Users />} />
+        </Route>
       </Routes>
     </div>
   );
