7d6e6c759dcfaf3de7295cff2cc56ea359b906ba
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-07 12:09:55 +0100

    An update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import AboutUs from "./components/about";
 import Navbar from "./components/navbar"
@@ -8,18 +8,25 @@ import Users from "./components/users"
 import NotFound from "./components/NotFound";
 import UserProfile from "./components/userProfile"
 import SearchUser from "./components/searchUser"
+import Login from "./components/login"
+import AuthProfile from "./components/authProfile"
+import { useState } from "react";
 function App() {
+  const [username, setUsername] = useState("");
+  const [isLogged, setIsLogged] = useState(false);
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Navbar/>}>
+        <Route path="/" element={<Navbar />}>
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
           <Route path="/users" element={<Users />} />
-          <Route path="/users/user/:username" element={<UserProfile/>}/>
-          <Route path="/search" element={<SearchUser/>}/>
+          <Route path="/users/user/:username" element={<UserProfile />} />
+          <Route path="/search" element={<SearchUser />} />
+          <Route path="/login" element={<Login setIsLogged={setIsLogged} setUsername={setUsername} />} />
+          <Route path="/authprofile" element={isLogged ? <AuthProfile username={username}/> : <Navigate replace to={"/login"} />} />
         </Route>
-        <Route path="*" element={<NotFound/>}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </div>
   );
