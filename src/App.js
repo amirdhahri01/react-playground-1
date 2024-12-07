2ca88e19f11091e7a78a28924e6baa6e97010975
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-07 14:14:20 +0100

    An update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Navigate, Route, Routes , useLocation} from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Home from "./components/home";
-import AboutUs from "./components/about";
-import Navbar from "./components/navbar"
-import Users from "./components/users"
-import NotFound from "./components/NotFound";
-import UserProfile from "./components/userProfile"
-import SearchUser from "./components/searchUser"
-import Login from "./components/login"
-import AuthProfile from "./components/authProfile"
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { CSSTransition, SwitchTransition } from "react-transition-group";
-const Users = React.lazy(() => import("./components/users"))
+const Users = lazy(() => import("./components/users"))
+const AboutUs = lazy(() => import("./components/about"))
+const NotFound = lazy(() => import("./components/NotFound"))
+const UserProfile = lazy(() => import("./components/userProfile"))
+const SearchUser = lazy(() => import("./components/searchUser"))
+const Login = lazy(() => import("./components/login"))
+const AuthProfile = lazy(() => import("./components/authProfile"))
+
 function App() {
   const [username, setUsername] = useState("");
   const [isLogged, setIsLogged] = useState(false);
@@ -21,7 +20,8 @@ function App() {
     <div className="App">
       <SwitchTransition component={null}>
         <CSSTransition key={location.pathname} classNames={"fade"} timeout={300} unmountOnExit >
-          <Routes location={location}>
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Routes location={location}>
               <Route exact path="/" element={<Home />} />
               <Route path="/about" element={<AboutUs />} />
               <Route path="/users" element={<Users />} />
@@ -29,8 +29,9 @@ function App() {
               <Route path="/search" element={<SearchUser />} />
               <Route path="/login" element={<Login setIsLogged={setIsLogged} setUsername={setUsername} />} />
               <Route path="/authprofile" element={isLogged ? <AuthProfile username={username} /> : <Navigate replace to={"/login"} />} />
-            <Route path="*" element={<NotFound />}></Route>
-          </Routes>
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </Suspense>
         </CSSTransition>
       </SwitchTransition>
     </div>
