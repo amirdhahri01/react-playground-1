5473f60f003baf4bbfde896ec579d25d522d5e15
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-07 12:25:53 +0100

    An update

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
       <SwitchTransition component={null}>
         <CSSTransition key={location.pathname} classNames={"fade"} timeout={300} unmountOnExit >
           <Routes location={location}>
-            <Route path="/" element={<Navbar />}>
               <Route exact path="/" element={<Home />} />
               <Route path="/about" element={<AboutUs />} />
               <Route path="/users" element={<Users />} />
@@ -29,7 +28,6 @@ function App() {
               <Route path="/search" element={<SearchUser />} />
               <Route path="/login" element={<Login setIsLogged={setIsLogged} setUsername={setUsername} />} />
               <Route path="/authprofile" element={isLogged ? <AuthProfile username={username} /> : <Navigate replace to={"/login"} />} />
-            </Route>
             <Route path="*" element={<NotFound />}></Route>
           </Routes>
         </CSSTransition>
